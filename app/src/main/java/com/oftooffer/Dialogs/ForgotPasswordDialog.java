71f39319b043b63c06f716f7a0344d507057e452
dharavp <pateldhara248@gmail.com>
2017-04-05 18:03:56 +0530

    add forgot password dialog

---
+++ app/src/main/java/com/oftooffer/Dialogs/ForgotPasswordDialog.java
@@ -0,0 +1,8 @@
+package com.oftooffer.Dialogs;
+
+/**
+ * Created by dsk-221 on 5/4/17.
+ */
+
+public class ForgotPasswordDialog {
+}
--- app/src/main/java/com/oftooffer/Fragments/CustomerRegistrationFragment.java
+++ app/src/main/java/com/oftooffer/Fragments/CustomerRegistrationFragment.java
@@ -118,4 +118,3 @@ public class CustomerRegistrationFragment extends Fragment implements UserRegist
 
     }
 }
-
